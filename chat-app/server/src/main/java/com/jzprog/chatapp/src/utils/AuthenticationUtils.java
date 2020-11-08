4de96698872afdaf2363bd7c1c20dc42155d20aa
giannis <john_z94@hotmail.com>
2020-11-09 01:41:38 +0200

    replace DatatypeConverter printHexBinary with encodeHexString of apache commons codec

--- chat-app/server/src/main/java/com/jzprog/chatapp/src/utils/AuthenticationUtils.java
+++ chat-app/server/src/main/java/com/jzprog/chatapp/src/utils/AuthenticationUtils.java
@@ -2,12 +2,13 @@ package com.jzprog.chatapp.src.utils;
 
 import java.security.MessageDigest;
 import javax.xml.bind.DatatypeConverter;
+import org.apache.commons.codec.binary.Hex;
 
 public class AuthenticationUtils {
 	
 	public static String getHashedPassword(String plainTextPassword) throws Exception {
 		MessageDigest md = MessageDigest.getInstance("MD5");
 	    md.update(plainTextPassword.getBytes());
-	    return DatatypeConverter.printHexBinary(md.digest()).toUpperCase();
+	    return Hex.encodeHexString(md.digest()).toUpperCase();
 	}
 }
