05c6ce3124c7f99b11055bccdc75066f05ad1a1e
giannis <john_z94@hotmail.com>
2020-11-09 01:45:41 +0200

    refactor: remove unused import

--- chat-app/server/src/main/java/com/jzprog/chatapp/src/utils/AuthenticationUtils.java
+++ chat-app/server/src/main/java/com/jzprog/chatapp/src/utils/AuthenticationUtils.java
@@ -1,7 +1,6 @@
 package com.jzprog.chatapp.src.utils;
 
 import java.security.MessageDigest;
-import javax.xml.bind.DatatypeConverter;
 import org.apache.commons.codec.binary.Hex;
 
 public class AuthenticationUtils {
