008f96d897025c7ffa1bacf69d224f183b39c736
giannis <john_z94@hotmail.com>
2020-11-19 00:51:52 +0200

    possible fix of blob

--- chat-app/server/src/main/java/com/jzprog/chatapp/src/database/UsersRepository.java
+++ chat-app/server/src/main/java/com/jzprog/chatapp/src/database/UsersRepository.java
@@ -6,15 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import com.jzprog.chatapp.src.model.User;
 
 @Repository
+@Transactional
 public interface UsersRepository extends CrudRepository<User,Integer> {
 
   User findUserByUsernameAndPassword(String username, String password);
   User findUserByUsername(String username);
   User findUserById(Integer id);
-  
+
   @Query("from User u where u.username like  %:str%")
   public List<User> findUsersMachingString(@Param("str") String str);
 }
