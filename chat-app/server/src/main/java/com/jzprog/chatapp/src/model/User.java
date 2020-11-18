6e5ce5a77a393b036989592a8c3de974f27a9bc7
giannis <john_z94@hotmail.com>
2020-11-19 00:01:46 +0200

    corrected Type import

--- chat-app/server/src/main/java/com/jzprog/chatapp/src/model/User.java
+++ chat-app/server/src/main/java/com/jzprog/chatapp/src/model/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Type;
+import org.hibernate.annotations.Type;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 
