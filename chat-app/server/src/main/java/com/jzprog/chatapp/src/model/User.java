8650c643cffbdaeafb33a6246526ca3d6c433200
giannis <john_z94@hotmail.com>
2020-11-18 23:51:45 +0200

    type import addition

--- chat-app/server/src/main/java/com/jzprog/chatapp/src/model/User.java
+++ chat-app/server/src/main/java/com/jzprog/chatapp/src/model/User.java
@@ -12,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Type;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 
