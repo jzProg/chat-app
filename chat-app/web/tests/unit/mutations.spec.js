9bb9a0886ea9f70bd01ed77f7522102dcf89f6c6
jzProg <john_z94@hotmail.com>
2021-04-25 16:40:39 +0300

    test: mutation tests added

--- chat-app/web/tests/unit/mutations.spec.js
+++ chat-app/web/tests/unit/mutations.spec.js
@@ -5,7 +5,34 @@
 */
 import store from '@/store/index';
 
-test('"setUserImage" update "state.userInfo.image" correctly', () => {
+test('"setUserImage" updates "state.userInfo.image" correctly', () => {
   store.commit('setUserImage', { value: 'https://www.google.com' });
   expect(store.state.userInfo.image).toBe('https://www.google.com');
+});
+
+test('"setPersonalInfo" updates "state.userInfo" correctly', () => {
+  const infoObject = {
+    id: 1,
+    image: 'https://www.google.com',
+    loginUsername: 'testUser',
+    email: 'jz@test.com',
+    messages: []
+  };
+  store.commit('setPersonalInfo', { value: infoObject });
+  expect(store.state.userInfo).toMatchObject(infoObject);
+
+  const desiredInfoObject = {
+    email: 'jz@test2.com'
+  };
+  store.commit('setPersonalInfo', { value: desiredInfoObject });
+  expect(store.state.userInfo).not.toMatchObject(infoObject);
+  expect(store.state.userInfo).toMatchObject(desiredInfoObject);
+  expect(store.state.userInfo.loginUsername).toBe('testUser');
+  expect(store.state.userInfo.id).toBe(1);
+  expect(store.state.userInfo.messages).toEqual([]);
+});
+
+test('"setPushNotificationPublicKey" updates "state.pushNotificationPublicKey" correctly', () => {
+  store.commit('setPushNotificationPublicKey', { value: '1234' });
+  expect(store.state.pushNotificationPublicKey).toBe('1234');
 });
